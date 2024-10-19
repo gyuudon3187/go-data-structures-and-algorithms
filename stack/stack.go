ae1bbc170d6a867eb3dca00a2097422afc95a851
Daniel Dahlberg <ddahlb@kth.se>
2024-10-20 00:34:29 +0200

    Added mutex to Pop() of the stack implementation.

--- stack/stack.go
+++ stack/stack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "sync"
+
 type stackItem struct {
 	item interface{}
 	next *stackItem
@@ -7,6 +9,7 @@ type stackItem struct {
 
 type stack struct {
 	sp *stackItem
+	mu sync.Mutex
 }
 
 func New() *stack {
@@ -18,6 +21,9 @@ func (s *stack) Push(item interface{}) {
 }
 
 func (s *stack) Pop() interface{} {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if s.sp.next != nil {
 		item := s.sp.item
 		s.sp = s.sp.next
