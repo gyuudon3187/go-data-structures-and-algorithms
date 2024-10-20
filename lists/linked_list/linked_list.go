3e206962da657c609f9cc15fda8b328454c495dc
Daniel Dahlberg <ddahlb@kth.se>
2024-10-20 09:16:34 +0200

    Added implementation of the linked list's Prepend().

---
+++ lists/linked_list/linked_list.go
@@ -0,0 +1,19 @@
+package linkedlist
+
+type linkedListItem struct {
+	item interface{}
+	prev *linkedListItem
+}
+
+type linkedList struct {
+	head *linkedListItem
+	tail *linkedListItem
+}
+
+func (l *linkedList) Prepend(item interface{}) {
+	if l.head == nil {
+		l.head = &linkedListItem{item: item}
+	} else {
+		l.head = &linkedListItem{item: item, prev: l.head}
+	}
+}
