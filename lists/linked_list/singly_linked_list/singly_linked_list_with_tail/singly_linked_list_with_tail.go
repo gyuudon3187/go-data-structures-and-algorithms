722bbac295f91f3b2c09283e9ad4b21355f0e711
Daniel Dahlberg <ddahlb@kth.se>
2024-10-20 12:20:09 +0200

    Added Append() to linked list implementation.

--- lists/linked_list/singly_linked_list/singly_linked_list_with_tail/singly_linked_list_with_tail.go
+++ lists/linked_list/singly_linked_list/singly_linked_list_with_tail/singly_linked_list_with_tail.go
@@ -12,9 +12,22 @@ type linkedList struct {
 
 func (l *linkedList) Prepend(item interface{}) {
 	if l.head == nil {
-		l.head = &linkedListItem{item: item}
-		l.tail = l.head
+		l.addFirstItem(item)
 	} else {
 		l.head = &linkedListItem{item: item, prev: l.head}
 	}
+}
+
+func (l *linkedList) Append(item interface{}) {
+	if l.head == nil {
+		l.addFirstItem(item)
+	} else {
+		l.tail.prev = &linkedListItem{item: item, prev: nil}
+		l.tail = l.tail.prev
+	}
+}
+
+func (l *linkedList) addFirstItem(item interface{}) {
+	l.head = &linkedListItem{item: item}
+	l.tail = l.head
 }
